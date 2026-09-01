ed18d9eb6133e8e5bcec6aa823c795a1418efe9d
Nishanth964 <nishanthnaik964@gmail.com>
2026-09-01 21:39:14 +0530

    added extra test function for game class

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,5 +1,34 @@
+import pygame
+
 from game import Game
 
+
+def test_window_size():
+    game = Game()
+
+    assert game.width == 800
+    assert game.height == 950
+
+    pygame.quit()
+
+
+def test_simulation_area():
+    game = Game()
+
+    assert game.simulationArea.width == 800
+    assert game.simulationArea.height == 800
+    assert game.simulationArea.topleft == (0, 0)
+
+    pygame.quit()
+
+
+def test_grid_size():
+    game = Game()
+
+    assert game.gridSize == 200
+    assert game.cellSize == 4
+
+    pygame.quit()
 
 def test_grid_size():
     game = Game()
