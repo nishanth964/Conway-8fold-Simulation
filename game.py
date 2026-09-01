b9359c35d616aa433f04a3e5e6cde9b5858252c9
Nishanth964 <nishanthnaik964@gmail.com>
2026-09-01 12:12:44 +0530

    added comments and format the code

--- game.py
+++ game.py
@@ -1,21 +1,28 @@
 import pygame
 
+
 class Game:
     def __init__(self):
         pygame.init()
-        self.WIDTH = 800
-        self.HEIGHT = 950
-        self.WINDOW = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
-        pygame.display.set_caption("Conway: 8Fold Simulation") 
+
+        # Set up the game window.
+        self.width = 800
+        self.height = 950
+        self.window = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption("Conway: 8Fold Simulation")
+
         self.clock = pygame.time.Clock()
 
     def run(self):
         self.running = True
+
         while self.running:
+            # Handle events.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
 
+            # Display the current frame.
             pygame.display.flip()
             self.clock.tick(60)
 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from game import Game
 
+
+# Create and run the game.
 game = Game()
 game.run()
-
 
